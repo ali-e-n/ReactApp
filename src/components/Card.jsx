8807627fe885a106391f0172ad622fe449ca988b
Ali Akbar Khan <aliakbarkhan161@gmail.com>
2024-10-21 19:58:41 +0500

    App file cleaned up and now everything has it own components

--- src/components/Card.jsx
+++ src/components/Card.jsx
@@ -1,6 +1,6 @@
 import React from "react";
 
-const card = ({ children, bg }) => {
+const card = ({ children, bg = "bg-gray-100" }) => {
   return <div className={`${bg} p-6 rounded-lg shadow-md`}>{children}</div>;
 };
 
